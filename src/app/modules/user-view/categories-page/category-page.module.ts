1d42102987be418e00cbef5362ed0d74843cb809
daotungsl <daotungsl@gmail.com>
2019-10-18 03:59:07 +0700

    push

--- src/app/modules/user-view/categories-page/category-page.module.ts
+++ src/app/modules/user-view/categories-page/category-page.module.ts
@@ -4,9 +4,6 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ClipboardModule } from 'ngx-clipboard';
 import { CategoryPageRoutes } from './category-page.routing';
-import { CategoryPageTopComponent } from './category-page-top/category-page-top.component';
-import { CategoryMainContentComponent } from './category-main-content/category-main-content.component';
-import { CategoryPageComponent } from './category-page/category-page.component';
 
 @NgModule({
 
@@ -20,15 +17,8 @@ import { CategoryPageComponent } from './category-page/category-page.component';
 
     ],
     declarations: [
-        CategoryPageComponent,
-        CategoryPageTopComponent,
-        CategoryMainContentComponent
+     
     ]
-    ,
-    exports: [
-        CategoryPageComponent,
-        CategoryPageTopComponent,
-        CategoryMainContentComponent
-    ]
+    
 })
 export class CategoryPageModule { }
